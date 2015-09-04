11b00f48e15a83b794e24da82b04d73a8c09d4ba
Patrick O'Leary <Patrick.1002.pol@gmail.com>
2015-09-04 16:00:48 -0700

    Completed files

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,12 +1,33 @@
 /*
- * name: LinkIt
+ *name: LinkIt
  * author: Patrick O'Leary
  * version: 0.1.0
  * license: -
 */
 
 (function($){
-	$.fn.linkIt = function(){
-		alert(1);
+	$.fn.linkIt = function(options){
+		//Default Settings 
+		var settings = $.extend({
+			href		:null,
+			text		:null,
+			target		:'_self'
+		}, options);
+		
+		//Validation 
+		if(settings.href == null){
+			console.log('You need an href option for LinkIt to work');
+			return this; 
+		}
+		
+		return this.each(function(){
+			var object = $(this);
+			
+			if(settings.text == null){
+				settings.text = object.text();
+			}
+			
+			object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+		});
 	}
 }(jQuery));
